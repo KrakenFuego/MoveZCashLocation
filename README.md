7156385baef7f5eb5c028898984541023221c44e
KrakenFuego <31135312+KrakenFuego@users.noreply.github.com>
2018-06-17 15:14:42 +0100

    Add files via upload
    Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Move Zcash Location
+Find my Blog @ https://krakenfuego.com
+
+Quick and Dirty Powershell script to move the location of the ZCash Blockchain
+
+Just a quick and dirty script I made to move the WinZec wallet location (via symlinking).
+
+It will try to create any folders if they dont exist (if you have used before running the wallet for the first time), or try to move existing folders and then symlink to them.
+
+Unzip, and Run with Powershell (Backup your wallet.dat / keys first just incase)
+
+~~
+
+Disclaimer: There is no warranty of any kind, don't blame me if it goes wrong and fubars your install :)
+
+~~
+
+Enter New Directory in the form G:/Folder
+
+Known feature – If you run without admin perms - After Script has popped up an admin window it will leave the old window saying Input New Location – Just close it
+
+If it helped you consider sending me a donation 
+
+~~
+
+Zec Donation Address: t1T8LwmHrGShAU2ZLmj3RzPmnjLe6GGobsh
---
+++ moveZcashLocation.ps1
@@ -0,0 +1,39 @@
+﻿# Move ZCash Wallet Location Script
+# Ⓒ 2018 @Krakenfuego
+# MIT License
+
+# Elevate Admin Permissions
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs}
+
+# Ask user where they want to store blockchain
+    $newBlockchainLocation = Read-Host -Prompt 'Input new location'
+
+# Try to Create the directory if it doesnt exist
+    New-Item $newBlockchainLocation -ItemType Directory
+    New-Item $newBlockchainLocation/Zcash -ItemType Directory
+    New-Item $newBlockchainLocation/ZcashParams -ItemType Directory    
+
+# Check If ZCash/ZCashParams Exit
+    $blockchainExists = 0;
+    $Zcash = "$env:AppData/Zcash"
+
+    If (Test-Path $Zcash) {
+        $blockchainExists = 1;
+        Write-Output "Zcash Installed"
+    }
+    Else {
+        Write-Output "No ZCash Install"
+    }
+
+# If Exists Move to New Location
+    If ($blockchainExists -eq 1) {
+        Move-Item -Path "$env:AppData/Zcash" -Destination $newBlockchainLocation
+        New-Item -Path "$env:AppData/Zcash" -ItemType SymbolicLink -Value $newBlockchainLocation/Zcash
+        Move-Item -Path "$env:AppData/ZcashParams" -Destination $newBlockchainLocation
+        New-Item -Path "$env:AppData/ZcashParams" -ItemType SymbolicLink -Value $newBlockchainLocation/ZCashParams
+    }
+    Else {
+        New-Item -Path "$env:AppData/Zcash" -ItemType SymbolicLink -Value $newBlockchainLocation/Zcash
+        New-Item -Path "$env:AppData/ZcashParams" -ItemType SymbolicLink -Value $newBlockchainLocation/ZCashParams
+    }
+
